77d6a20bf3268ec6dcf896d4dd2e1687a41bf7cf
YamiTeyssier <97235275+YamiTeyssier@users.noreply.github.com>
2023-11-01 12:49:34 -0300

    Add delivery example app

---
+++ delivery-client/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_yama_plugins/pages/example_home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      //   useMaterial3: true,
+      // ),
+      //
+      // theme: ThemeData.dark(
+      //   useMaterial3: true,
+      // ),
+      //
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.amber,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      // home: const MyHomePage(),
+      home: const ExampleHomePage(),
+    );
+  }
+}
